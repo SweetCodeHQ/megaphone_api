8dff44ccb5c0acb7b3f0febec688e742d9321800
Robert Heath <robert.b.heathii@gmail.com>
2022-05-24 14:12:02 -0500

    Write first user test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+require 'rails_helper'
+
+describe User do
+  describe 'relationships' do
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :email }
+  end
+end
