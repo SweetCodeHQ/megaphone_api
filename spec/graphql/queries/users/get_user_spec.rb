8e4ff1f24c2c54eee0677c21fd0a1e93329ecd7d
Robert Heath <robert.b.heathii@gmail.com>
2022-05-25 08:57:48 -0500

    Update user query tests with sad path

--- spec/graphql/queries/users/get_user_spec.rb
+++ spec/graphql/queries/users/get_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include GraphQL::TestHelpers
 
 RSpec.describe Types::QueryType, type: :request do
-  describe 'get employees' do
+  describe 'get user' do
     let(:user) { create(:user) }
 
     let(:query_type_one) { "user" }
@@ -17,22 +17,36 @@ RSpec.describe Types::QueryType, type: :request do
     GQL
     }
     describe "return one user" do
-      before do
-        user
-        query query_string_one, variables: { email: "#{user.email}" }
+      describe "happy path" do
+        before do
+          user
+          query query_string_one, variables: { email: "#{user.email}" }
+        end
+
+        it 'should return no errors' do
+          expect(gql_response.errors).to be_nil
+        end
+
+        it 'should return one user' do
+          expect(gql_response.data["user"]).to be_a Hash
+          expect(gql_response.data["user"]).to eq({
+            "id" => user.id.to_s,
+            "email" => user.email,
+            "isAdmin" => user.is_admin
+          })
+        end
       end
 
-      it 'should return no errors' do
-        expect(gql_response.errors).to be_nil
-      end
-
-      it 'should return one user' do
-        expect(gql_response.data["user"]).to be_a Hash
-        expect(gql_response.data["user"]).to eq({
-          "id" => user.id.to_s,
-          "email" => user.email,
-          "isAdmin" => user.is_admin
-        })
+      describe "sad path" do
+        context "email does not exist" do
+          it 'should return an error' do
+            user
+            query query_string_one, variables: { email: "notAnEmail" }
+            
+            expect(gql_response.errors).to be_nil
+            expect(gql_response.data["user"]). to be_nil
+          end
+        end
       end
     end
   end
