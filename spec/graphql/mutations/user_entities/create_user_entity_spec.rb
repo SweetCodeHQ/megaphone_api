f257a1187096b25b831a4b4383e745fbdbda4179
Robert Heath <robert.b.heathii@gmail.com>
2022-06-07 10:45:50 -0500

    Test for user entity creation mutation

---
+++ spec/graphql/mutations/user_entities/create_user_entity_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+module Mutations
+  module UserEntities
+    RSpec.describe CreateUserEntity, type: :request do
+      describe '.resolve' do
+        it 'creates a user entity' do
+          user   = create(:user)
+          entity = create(:entity)
+
+          expect do
+            post '/graphql', params:
+              { query: g_query(user_id: user.id, entity_id: entity.id)
+              }
+          end.to change { UserEntity.count }.by(1)
+        end
+
+        it 'returns a user entity' do
+          user   = create(:user)
+          entity = create(:entity)
+
+          post '/graphql', params: { query: g_query(user_id: user.id, entity_id: entity.id) }
+
+          json = JSON.parse(response.body, symbolize_names: true)
+          data = json[:data][:createUserEntity]
+
+          expect(data).to include(
+            id: "#{UserEntity.first.id}"
+          )
+        end
+
+        def g_query(user_id:, entity_id:)
+          <<~GQL
+            mutation {
+              createUserEntity( input: {
+                userId: "#{user_id}"
+                entityId: "#{entity_id}"
+              } ){
+                id
+              }
+            }
+          GQL
+        end
+      end
+
+      describe 'sad path' do
+        it 'returns errors' do
+          user   = create(:user)
+          entity = create(:entity)
+
+          post '/graphql', params: { query: g_query(user_id: user.id, entity_id: entity.id) }
+
+          json = JSON.parse(response.body, symbolize_names: true)
+          expect(json).to have_key(:errors)
+        end
+
+        def g_query(user_id:, entity_id:)
+          <<~GQL
+            mutation {
+              createUserEntity( input: {
+                userId: "#{user_id}"
+              } ){
+                id
+              }
+            }
+          GQL
+        end
+      end
+    end
+  end
+end
