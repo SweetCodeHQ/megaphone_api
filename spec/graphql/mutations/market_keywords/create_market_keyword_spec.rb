3b7c980c303d16e71bcae1a70a68cd39c6afd780
Robert Heath <robert.b.heathii@gmail.com>
2022-06-09 12:12:49 -0500

    Write tests for mutation for joins objects

---
+++ spec/graphql/mutations/market_keywords/create_market_keyword_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+module Mutations
+  module MarketKeywords
+    RSpec.describe CreateMarketKeyword, type: :request do
+      describe '.resolve' do
+        it 'creates a user keyword' do
+          market  = create(:market)
+          keyword = create(:keyword)
+
+          expect do
+            post '/graphql', params:
+              { query: g_query(market_id: market.id, keyword_id: keyword.id)
+              }
+          end.to change { MarketKeyword.count }.by(1)
+        end
+
+        it 'returns a user keyword' do
+          market = create(:market)
+          keyword = create(:keyword)
+
+          post '/graphql', params: { query: g_query(market_id: market.id, keyword_id: keyword.id) }
+
+          json = JSON.parse(response.body, symbolize_names: true)
+          data = json[:data][:createMarketKeyword]
+
+          expect(data).to include(
+            id: "#{MarketEntity.first.id}"
+          )
+        end
+
+        def g_query(market_id:, keyword_id:)
+          <<~GQL
+            mutation {
+              createUserEntity( input: {
+                marketId: "#{market_id}"
+                keywordId: "#{keyword_id}"
+              } ){
+                id
+              }
+            }
+          GQL
+        end
+      end
+
+      describe 'sad path' do
+        it 'returns errors' do
+          user   = create(:user)
+          keyword = create(:keyword)
+
+          post '/graphql', params: { query: g_query(market_id: market.id, keyword_id: keyword.id) }
+
+          json = JSON.parse(response.body, symbolize_names: true)
+          expect(json).to have_key(:errors)
+        end
+
+        def g_query(market_id:, keyword_id:)
+          <<~GQL
+            mutation {
+              createUserEntity( input: {
+                marketId: "#{market_id}"
+              } ){
+                id
+              }
+            }
+          GQL
+        end
+      end
+    end
+  end
+end
