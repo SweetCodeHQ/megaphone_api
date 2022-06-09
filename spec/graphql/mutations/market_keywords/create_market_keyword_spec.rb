056bf2842f70bbc1b7f6540bdd5371be90747bbb
Robert Heath <robert.b.heathii@gmail.com>
2022-06-09 12:28:15 -0500

    Update test

--- spec/graphql/mutations/market_keywords/create_market_keyword_spec.rb
+++ spec/graphql/mutations/market_keywords/create_market_keyword_spec.rb
@@ -4,7 +4,7 @@ module Mutations
   module MarketKeywords
     RSpec.describe CreateMarketKeyword, type: :request do
       describe '.resolve' do
-        it 'creates a user keyword' do
+        it 'creates a market keyword' do
           market  = create(:market)
           keyword = create(:keyword)
 
@@ -15,7 +15,7 @@ module Mutations
           end.to change { MarketKeyword.count }.by(1)
         end
 
-        it 'returns a user keyword' do
+        it 'returns a market keyword' do
           market = create(:market)
           keyword = create(:keyword)
 
@@ -25,14 +25,14 @@ module Mutations
           data = json[:data][:createMarketKeyword]
 
           expect(data).to include(
-            id: "#{MarketEntity.first.id}"
+            id: "#{MarketKeyword.first.id}"
           )
         end
 
         def g_query(market_id:, keyword_id:)
           <<~GQL
             mutation {
-              createUserEntity( input: {
+              createMarketKeyword( input: {
                 marketId: "#{market_id}"
                 keywordId: "#{keyword_id}"
               } ){
@@ -45,7 +45,7 @@ module Mutations
 
       describe 'sad path' do
         it 'returns errors' do
-          user   = create(:user)
+          market  = create(:market)
           keyword = create(:keyword)
 
           post '/graphql', params: { query: g_query(market_id: market.id, keyword_id: keyword.id) }
