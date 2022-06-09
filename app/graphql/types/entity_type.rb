d3b10e561f36ea69e1206b71784a11a7031e7af0
Robert Heath <robert.b.heathii@gmail.com>
2022-06-09 11:45:28 -0500

    Update types for joins objects

--- app/graphql/types/entity_type.rb
+++ app/graphql/types/entity_type.rb
@@ -1,9 +1,14 @@
 module Types
   class EntityType < Types::BaseObject
+    field :markets, [Types::MarketType], null: true
 
     field :id, ID, null: false
 
     field :url,    String,  null: false
     field :name,   String,  null: false
+
+    def markets
+      Loaders::AssociationLoader.for(object.class, :markets).load(object)
+    end
   end
 end
--- app/graphql/types/keyword_type.rb
+++ app/graphql/types/keyword_type.rb
@@ -1,12 +1,12 @@
 module Types
   class KeywordType < Types::BaseObject
-    # field :entities, [Types::EntityType], null: true
+    field :markets, [Types::MarketType], null: true
 
     field :id,      ID,      null: false
     field :word,    String,  null: false
 
-    # def entities
-    #   Loaders::AssociationLoader.for(object.class, :entities).load(object)
-    # end
+    def entities
+      Loaders::AssociationLoader.for(object.class, :markets).load(object)
+    end
   end
 end
