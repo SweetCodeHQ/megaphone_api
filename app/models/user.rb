6256f26c01b8d4658a30735f8e1e2c313a9ecf39
Robert Heath <robert.b.heathii@gmail.com>
2022-06-21 15:03:34 -0500

    Update user to have many topics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_entities
   has_many :entities, through: :user_entities
+  has_many :topics
+  
   validates_presence_of :email
   validates :email, uniqueness: true
 end
