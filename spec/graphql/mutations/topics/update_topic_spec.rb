4aa9b7dac2e66a4f8091fc72a594277aa445b54f
Robert Heath <robert.b.heathii@gmail.com>
2022-06-21 15:34:06 -0500

    Update topics tests

---
+++ spec/graphql/mutations/topics/update_topic_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+module Mutations
+  module Topics
+    RSpec.describe Topic, type: :request do
+      describe 'resolve' do
+        it 'updates a topic' do
+          user = create(:user)
+          topic = create(:topic)
+
+          post '/graphql', params: { query: g_query(id: topic.id) }
+
+          expect(topic.reload).to have_attributes(
+            text: "New Text"
+          )
+        end
+
+        it 'returns a topic' do
+          user = create(:user)
+          topic2 = create(:topic)
+
+          post '/graphql', params: { query: g_query(id: topic2.id) }
+          json = JSON.parse(response.body, symbolize_names: true)
+          data = json[:data][:updateTopic]
+
+          expect(data).to include(
+            id: "#{ topic2.reload.id }",
+            text: "New Text"
+          )
+        end
+
+        def g_query(id:)
+          <<~GQL
+            mutation {
+              updateTopic(input: {
+                id: #{id}
+                text: "New Text"
+              }){
+                id
+                text
+              }
+            }
+          GQL
+        end
+      end
+
+      describe 'sad path' do
+        it 'returns with errors' do
+          user = create(:user)
+          topic3 = create(:topic)
+
+          post '/graphql', params: { query: g_query(id: 'not an id') }
+          json = JSON.parse(response.body, symbolize_names: true)
+          expect(json).to have_key(:errors)
+        end
+
+        def g_query(id:)
+          <<~GQL
+            mutation {
+              updateTopic( input: {
+                id: 'not an id'
+              }) {
+                id
+                name
+              }
+            }
+          GQL
+        end
+      end
+    end
+  end
+end
