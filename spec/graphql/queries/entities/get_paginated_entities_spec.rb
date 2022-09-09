ce224716dbf42cfcf66a96c3814918c6792441c0
Robert Heath <robert.b.heathii@gmail.com>
2022-09-09 09:27:25 -0500

    add addtional tests for pagination forwards and backwards

--- spec/graphql/queries/entities/get_paginated_entities_spec.rb
+++ spec/graphql/queries/entities/get_paginated_entities_spec.rb
@@ -3,24 +3,92 @@ include GraphQL::TestHelpers
 
 RSpec.describe Types::QueryType, type: :request do
   describe '.resolve' do
-    it 'returns all books with pagination' do
-      create(:entity)
-      create(:entity)
-      create(:entity)
+    before do
+      create_list(:entity, 15)
+    end
 
+    it 'returns first page' do
       post '/graphql', params: { query: query }
       json = JSON.parse(response.body)
 
       page_info = json['data']['entitiesConnection']['pageInfo']
+      entities = json['data']['entitiesConnection']['edges']
 
-      expect(page_info).to eq({"endCursor"=>"Mg", "startCursor"=>"MQ", "hasPreviousPage"=>false, "hasNextPage"=>true})
+      expect(page_info).to eq({"endCursor"=>"MTA", "startCursor"=>"MQ", "hasPreviousPage"=>false, "hasNextPage"=>true})
+      expect(entities.size).to eq(10)
+    end
+
+    it 'returns second page' do
+      post '/graphql', params: { query: query2 }
+      json = JSON.parse(response.body)
+
+      page_info = json['data']['entitiesConnection']['pageInfo']
+      entities = json['data']['entitiesConnection']['edges']
+
+      expect(page_info).to eq({"endCursor"=>"MTU", "startCursor"=>"MTE", "hasPreviousPage"=>true, "hasNextPage"=>false})
+      expect(entities.size).to eq(5)
+    end
+
+    it 'returns first page through backwards pagination' do
+      post '/graphql', params: { query: query3 }
+      json = JSON.parse(response.body)
+
+      page_info = json['data']['entitiesConnection']['pageInfo']
+      entities = json['data']['entitiesConnection']['edges']
+
+      expect(page_info).to eq({"endCursor"=>"MTA", "startCursor"=>"MQ", "hasPreviousPage"=>false, "hasNextPage"=>true})
+      expect(entities.size).to eq(10)
     end
   end
 
   def query
     <<~GQL
       query {
-        entitiesConnection(first: 2) {
+        entitiesConnection(first: 10) {
+          pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              name
+              url
+            }
+          }
+        }
+      }
+    GQL
+  end
+
+  def query2
+    <<~GQL
+      query {
+        entitiesConnection(after: "MTA") {
+          pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              name
+              url
+            }
+          }
+        }
+      }
+    GQL
+  end
+
+  def query3
+    <<~GQL
+      query {
+        entitiesConnection(before: "MTE") {
           pageInfo {
           endCursor
           startCursor
