acd5f8a84f7cc6f0020574651933b75396ca05e4
Robert Heath <robert.b.heathii@gmail.com>
2022-09-13 10:51:10 -0500

    Add is blocked to update user

--- app/graphql/mutations/users/update_user.rb
+++ app/graphql/mutations/users/update_user.rb
@@ -1,10 +1,11 @@
 module Mutations
   module Users
     class UpdateUser < ::Mutations::BaseMutation
-      argument :id,               ID,               required: true
-      argument :email,            String,           required: false
-      argument :is_admin,         Boolean,          required: false
-      
+      argument :id,          ID,         required: true
+      argument :email,       String,     required: false
+      argument :is_admin,    Boolean,    required: false
+      argument :is_blocked   Boolean,    required: false
+
       type Types::UserType
 
       def resolve(id:, **attributes)
