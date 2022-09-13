7d3c53247d3083384bd3ac587aba80254ebbb56c
Robert Heath <robert.b.heathii@gmail.com>
2022-09-13 09:24:10 -0500

    Update user with connection object for pagination

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,12 +1,13 @@
 module Types
   class UserType < Types::BaseObject
-    field :entities, [Types::EntityType], null: true
-    field :topics, [Types::TopicType], null: true
+    field :entities,  [Types::EntityType],  null: true
+    field :topics,    [Types::TopicType],   null: true
 
-    field :id, ID, null: false
+    field :id,          ID,      null: false
 
-    field :email,    String,  null: false
-    field :is_admin, Boolean, null: false
+    field :email,       String,  null: false
+    field :is_admin,    Boolean, null: false
+    field :is_blocked,  Boolean, null: false
 
     def entities
       Loaders::AssociationLoader.for(object.class, :entities).load(object)
---
+++ spec/graphql/queries/users/get_users_connection_spec.rb
@@ -0,0 +1,140 @@
+require 'rails_helper'
+include GraphQL::TestHelpers
+
+RSpec.describe Types::QueryType, type: :request do
+  describe '.resolve' do
+    before do
+      create_list(:user, 30)
+    end
+
+    it 'returns first page' do
+      post '/graphql', params: { query: query }
+      json = JSON.parse(response.body)
+
+      page_info = json['data']['usersConnection']['pageInfo']
+      users = json['data']['usersConnection']['edges']
+
+      expect(page_info).to eq({"endCursor"=>"MTA", "startCursor"=>"MQ", "hasPreviousPage"=>false, "hasNextPage"=>true})
+      expect(users.size).to eq(10)
+    end
+
+    it 'returns second page' do
+      post '/graphql', params: { query: query2 }
+      json = JSON.parse(response.body)
+
+      page_info = json['data']['usersConnection']['pageInfo']
+      users = json['data']['usersConnection']['edges']
+
+      expect(page_info).to eq({"endCursor"=>"MjA", "startCursor"=>"MTE", "hasPreviousPage"=>true, "hasNextPage"=>true})
+      expect(users.size).to eq(10)
+    end
+# Backwards pagination is implemented but doesn't work. Isolated to connection_type object. Will accept after argument but not 'before'. Unknown why.
+    it 'returns the third page' do
+      post '/graphql', params: { query: query3 }
+      json = JSON.parse(response.body)
+
+      page_info = json['data']['usersConnection']['pageInfo']
+      users = json['data']['usersConnection']['edges']
+
+      expect(page_info).to eq({"endCursor"=>"MzA", "startCursor"=>"MjE", "hasPreviousPage"=>true, "hasNextPage"=>false})
+      expect(users.size).to eq(10)
+    end
+    #
+    # it 'returns the second page through backwards pagination' do
+    #   post '/graphql', params: { query: query4 }
+    #   json = JSON.parse(response.body)
+    #
+    #   page_info = json['data']['usersConnection']['pageInfo']
+    #   users = json['data']['usersConnection']['edges']
+    #
+    #   expect(page_info).to eq({"endCursor"=>"MjA", "startCursor"=>"MTE", "hasPreviousPage"=>true, "hasNextPage"=>true})
+    #   expect(users.size).to eq(10)
+    # end
+  end
+
+  def query
+    <<~GQL
+      query {
+        usersConnection {
+          pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              id
+              email
+              isBlocked
+            }
+          }
+        }
+      }
+    GQL
+  end
+
+  def query2
+    <<~GQL
+      query {
+        usersConnection(after: "MTA") {
+          pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              email
+            }
+          }
+        }
+      }
+    GQL
+  end
+
+  def query3
+    <<~GQL
+      query {
+        usersConnection(after: "MjA") {
+          pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              email
+            }
+          }
+        }
+      }
+    GQL
+  end
+
+  def query4
+    <<~GQL
+      query {
+        usersConnection(before: "MjE") {
+          pageInfo {
+          endCursor
+          startCursor
+          hasPreviousPage
+          hasNextPage
+          }
+          edges {
+            cursor
+            node {
+              email
+            }
+          }
+        }
+      }
+    GQL
+  end
+end
