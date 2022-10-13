252b336fd51a78b70b8df51d620f5a4b60cc6016
Robert Heath <robert.b.heathii@gmail.com>
2022-10-13 13:27:55 -0500

    remove unneccessary arguments from the update mutation

--- app/graphql/mutations/keywords/update_keyword.rb
+++ app/graphql/mutations/keywords/update_keyword.rb
@@ -2,8 +2,6 @@ module Mutations
   module Keywords
     class UpdateKeyword < ::Mutations::BaseMutation
       argument :id,        ID,         required: true
-      argument :word,      String,     required: false
-      argument :search_count, Int,     required: false
 
       type Types::KeywordType
 # The only thing it updates is search_count
