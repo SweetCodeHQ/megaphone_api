f7619c2f9dcffc2aaf1030753ee1fdffbbbbdfe8
Robert Heath <robert.b.heathii@gmail.com>
2022-12-07 21:49:13 -0600

    Create abstract type, add abstracts to topic topicType

--- app/graphql/types/topic_type.rb
+++ app/graphql/types/topic_type.rb
@@ -1,8 +1,13 @@
 module Types
   class TopicType < Types::BaseObject
+    field :abstract, Types::AbstractType, null: true
 
     field :id,      ID,         null: false
     field :text,    String,     null: false
     field :submitted, Boolean,  null: false
+
+    def abstract
+      Loaders::AssociationLoader.for(object.class, :abstract).load(object)
+    end
   end
 end
