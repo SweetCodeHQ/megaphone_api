4ed6e42050e3625e6c82a356618ee957bf43cb86
Robert Heath <robert.b.heathii@gmail.com>
2023-05-12 13:02:05 -0500

    Remove uniquness validation because it is too specific

--- spec/models/topic_keyword_spec.rb
+++ spec/models/topic_keyword_spec.rb
@@ -5,14 +5,4 @@ describe TopicKeyword do
     it { should belong_to :topic }
     it { should belong_to :keyword }
   end
-
-  describe 'validations' do
-    before do
-      create(:user)
-      create(:topic)
-      create(:keyword)
-      create(:topic_keyword)
-    end
-    it { should validate_uniqueness_of :keyword }
-  end
 end
