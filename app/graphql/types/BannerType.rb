fb5f92742485d30877b622b313048565350d60f7
Robert Heath <robert.b.heathii@gmail.com>
2023-06-05 16:04:59 -0500

    Add Updated At to banners

--- app/graphql/types/BannerType.rb
+++ app/graphql/types/BannerType.rb
@@ -3,7 +3,8 @@ module Types
     field :id,        ID,         null: false
     field :text,      String,     null: false
     field :link,      String,    null: true
-    field :purpose,      Integer,    null: false
+    field :purpose,   Integer,    null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     def purpose
       Banner.purposes[@object.purpose]
