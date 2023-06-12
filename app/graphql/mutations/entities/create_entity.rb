cbfb0c5c57ed5bd977bdc0963f50fa07ff98ee3d
Robert Heath <robert.b.heathii@gmail.com>
2023-06-11 20:10:45 -0500

    Remove necessity for name at creation

--- app/graphql/mutations/entities/create_entity.rb
+++ app/graphql/mutations/entities/create_entity.rb
@@ -2,7 +2,7 @@ module Mutations
   module Entities
     class CreateEntity < ::Mutations::BaseMutation
       argument :url,     String,      required: true
-      argument :name,    String,      required: true
+      argument :name,    String,      required: false
 
       type Types::EntityType
 
