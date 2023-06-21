db64d2c417e5de00e9fd066beb4fd77b402fccd3
Robert Heath <robert.b.heathii@gmail.com>
2023-06-21 10:49:45 -0500

    Add strong params for controllers

--- app/controllers/topic_alert_emails_controller.rb
+++ app/controllers/topic_alert_emails_controller.rb
@@ -2,11 +2,10 @@ class TopicAlertEmailsController < ApplicationController
   require 'sendgrid-ruby'
 
   def create
-    # need to clean this up and add require/permit topic_params
-    topic = Topic.find(params["topic_id"])
+    topic = Topic.find(topic_params["topic_id"])
     user = topic.user
 
-    email_body = "#{user.email} has sent a request for the following topic: #{topic.text}. The abstract is as follows: #{topic.abstract.text}."
+    email_body = "#{user.email} has sent a request for the following topic: #{topic.text}. The abstract is as follows: #{topic.abstract&.text ? topic.abstract.text : "none generated"}."
 
     from = SendGrid::Email.new(email: 'robert@fixate.io')
     to = SendGrid::Email.new(email: ENV["ALERT_EMAIL_ADDRESS"])
@@ -17,5 +16,11 @@ class TopicAlertEmailsController < ApplicationController
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+
+  private
+
+  def topic_params
+    params.permit(:topic_id)
   end
 end
