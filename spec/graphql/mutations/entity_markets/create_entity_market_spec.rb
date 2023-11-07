824224f31fe829e68ffcf6e30e2d7afe9a29cab5
Robert Heath <robert.b.heathii@gmail.com>
2023-11-07 10:15:52 -0600

    Update tests

--- spec/graphql/mutations/entity_markets/create_entity_market_spec.rb
+++ spec/graphql/mutations/entity_markets/create_entity_market_spec.rb
@@ -11,7 +11,7 @@ module Mutations
           expect do
             post '/graphql', params:
               { query: g_query(market_id: market.id, entity_id: entity.id)
-              }
+              }, headers: { authorization: ENV['MUTATION_KEY'] }
           end.to change { EntityMarket.count }.by(1)
         end
 
@@ -19,7 +19,7 @@ module Mutations
           market = create(:market)
           entity = create(:entity)
 
-          post '/graphql', params: { query: g_query(market_id: market.id, entity_id: entity.id) }
+          post '/graphql', params: { query: g_query(market_id: market.id, entity_id: entity.id) }, headers: { authorization: ENV['MUTATION_KEY'] }
 
           json = JSON.parse(response.body, symbolize_names: true)
           data = json[:data][:createEntityMarket]
@@ -48,7 +48,7 @@ module Mutations
           market   = create(:market)
           entity = create(:entity)
 
-          post '/graphql', params: { query: g_query(market_id: market.id, entity_id: entity.id) }
+          post '/graphql', params: { query: g_query(market_id: market.id, entity_id: entity.id) }, headers: { authorization: ENV['MUTATION_KEY'] }
 
           json = JSON.parse(response.body, symbolize_names: true)
           expect(json).to have_key(:errors)
