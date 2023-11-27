6080a61aed1a70c3941974447c3a59064dbc294b
Robert Heath <robert.b.heathii@gmail.com>
2023-11-27 15:08:08 -0600

    Add the context requirements for user queries

--- spec/graphql/queries/users/get_fixate_users_spec.rb
+++ spec/graphql/queries/users/get_fixate_users_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Types::QueryType, type: :request do
   describe 'get entity for a user' do
     let(:user1)         { create(:user) }
     let(:user2)         { create(:user) }
+    let(:user3)         { create(:user, is_admin: true) }
     let(:entity)        { create(:entity, name: "Fixate") }
     let(:user_entity)   { create(:user_entity, user: user1) }
 
@@ -24,19 +25,26 @@ RSpec.describe Types::QueryType, type: :request do
       before do
         user1
         user2
+        user3
         entity
         user_entity
-        query query_string_all
+        post '/graphql', params: { query: query_string_all }, headers: { authorization: ENV['EAGLE_KEY'], user: user3.id }
       end
 
       it 'should return no errors' do
-        expect(gql_response.errors).to be_nil
+        json = JSON.parse(response.body)
+        data = json['data']
+
+        expect(data['errors']).to be_nil
       end
 
       it 'should return a list of admin users' do
-        expect(gql_response.data["fixateUsers"]).to be_an Array
-        expect(gql_response.data["fixateUsers"].size).to eq(1)
-        expect(gql_response.data["fixateUsers"].first["entities"].first["name"]).to eq("Fixate")
+        json = JSON.parse(response.body)
+        data = json['data']
+        
+        expect(data["fixateUsers"]).to be_an Array
+        expect(data["fixateUsers"].size).to eq(1)
+        expect(data["fixateUsers"].first["entities"].first["name"]).to eq("Fixate")
       end
     end
   end
