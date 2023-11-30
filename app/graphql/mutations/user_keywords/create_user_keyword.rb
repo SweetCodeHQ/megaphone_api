63106bbba61b60b03cb0f63b6fa88f22189f1bc2
Robert Heath <robert.b.heathii@gmail.com>
2023-11-30 09:42:33 -0600

    Pull userId from context

--- app/graphql/mutations/user_keywords/create_user_keyword.rb
+++ app/graphql/mutations/user_keywords/create_user_keyword.rb
@@ -1,15 +1,13 @@
 module Mutations
   module UserKeywords
     class CreateUserKeyword < ::Mutations::BaseMutation
-      argument :user_id,    ID, required: true
       argument :word, String, required: true
 
       type Types::UserKeywordType
 
       def resolve(**attributes)
-        raise GraphQL::ExecutionError, "Incorrect execution." if context[:current_user] != @prepared_arguments[:user_id].to_i
         keyword_id = Keyword.find_by(word: attributes[:word]).id
-        user_id = attributes[:user_id]
+        user_id = context[:current_user]
         user_keyword = UserKeyword.where(user_id: user_id).where(keyword_id: keyword_id).first
 
         user_keyword ?
