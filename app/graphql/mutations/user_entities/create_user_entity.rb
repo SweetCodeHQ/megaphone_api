5570b6359f41101a12d2bbfb2f40f567a0e26227
Robert Heath <robert.b.heathii@gmail.com>
2023-12-07 16:47:51 -0600

    Move user to context

--- app/graphql/mutations/user_entities/create_user_entity.rb
+++ app/graphql/mutations/user_entities/create_user_entity.rb
@@ -1,13 +1,14 @@
 module Mutations
   module UserEntities
     class CreateUserEntity < ::Mutations::BaseMutation
-      argument :user_id,      ID,     required: true
       argument :entity_id,    ID,     required: true
 
       type Types::UserEntityType
 
       def resolve(**attributes)
-        UserEntity.create!(attributes)
+        attrs = attributes
+        attrs[:user_id] = context[:current_user]
+        UserEntity.create!(attrs)
       end
     end
   end
